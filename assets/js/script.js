3580013c10ce6e6f97fc762c6d6d69ad69690fd0
m4sterpaul18 <cirerachristian@gmail.com>
2021-04-15 09:40:45 +0800

    final

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('#fb-toggle').on('mouseleave mouseover',()=>{
+		$('#fb').slideToggle(400);
+	});	
+	$('#mail-toggle').on('mouseleave mouseover',()=>{
+		$('#mail').slideToggle(400);
+	});	
+	$('#twitter-toggle').on('mouseleave mouseover',()=>{
+		$('#twit').slideToggle(400);
+	});	
+	$('#phone-toggle').on('mouseleave mouseover',()=>{
+		$('#phone').slideToggle(400);
+	});
+});
